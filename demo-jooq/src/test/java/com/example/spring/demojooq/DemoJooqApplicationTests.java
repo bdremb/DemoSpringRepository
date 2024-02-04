194dda8fd0a91567b6b5912d0519bd3f1c6498d4
Skillbox <bdremb@gmail.com>
2024-02-04 16:40:51 +0300

    init demo jooq

---
+++ demo-jooq/src/test/java/com/example/spring/demojooq/DemoJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.demojooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
